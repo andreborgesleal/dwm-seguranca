c8b448d8f6208c44096e155a71b7feaff29180ad
afbleal@gmail.com <afbleal@gmail.com>
2016-08-05 09:02:34 -0300

    backup notebook

--- Seguranca/Models/Enumeracoes/BindDropDownList.cs
+++ Seguranca/Models/Enumeracoes/BindDropDownList.cs
@@ -1,11 +1,8 @@
-﻿using App_Dominio.Contratos;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using App_Dominio.Entidades;
-using System.Data.Objects.SqlClient;
+//using System.Data.Objects.SqlClient;
 using Seguranca.Models.Entidades;
 using App_Dominio.Security;
 
